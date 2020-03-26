4404051f944dc4e018d77fbd558c4f2125736169
Luc <0988577@hr.nl>
2020-03-26 11:08:15 +0100

    worked on login

---
+++ loginsystem/users.py
@@ -0,0 +1,7 @@
+#["id", "pincode", failed login attempts]
+
+userTable = [ ["bob", "1234", 0], 
+              ["rick", "6669", 1], 
+              ["bob_2e", "4269", 2],
+              ["persoon3", "6543", 0] 
+]
